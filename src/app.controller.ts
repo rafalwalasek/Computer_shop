c1253553a6e5b316e77080b97771fb649492f80e
Rafal Walasek <magician1590@gmail.com>
2026-08-18 16:41:50 +0200

    Initial new project: computer shop

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('products')
+  getProducts() {
+    return 'Products';
+  }
+
+  @Post('products')
+  addProduct() {
+    return 'Product added'
+  }
+}
