9c4d1d3128a7e443d9f985cc5d060fb15e733e58
Rafal Walasek <magician1590@gmail.com>
2026-08-22 18:23:25 +0200

    wyszukiwarka na stronie - koniec

--- public/assets/js/products.js
+++ public/assets/js/products.js
@@ -35,12 +35,70 @@ const products = [
         price: 650,
         category: "M.2",
         available: true
+    },
+    {
+        id: 5,
+        image: "assets/images/gpu-amd-1.webp",
+        name: "Gigabyte Radeon RX 7600",
+        price: 1500,
+        category: "GPU",
+        available: true
+    },
+    {
+        id: 6,
+        image: "assets/images/mb-1.webp",
+        name: "ASUS TUF GAMING B850-PLUS WIFI",
+        price: 900,
+        category: "Motherboard",
+        available: true
+    },
+    {
+        id: 7,
+        image: "assets/images/cpu-intel-1.webp",
+        name: "Intel Core i5-14400F",
+        price: 750,
+        category: "CPU",
+        available: true
+    },
+    {
+        id: 8,
+        image: "assets/images/ram-2.webp",
+        name: "Kingston FURY 32GB",
+        price: 2250,
+        category: "RAM",
+        available: true
     }
 ];
 
 const cartCountElement = document.querySelector(".cart-count");
 
 products.forEach(product => {
+    const productCard = createProductCard(product);
+    productsGrid.appendChild(productCard);
+});
+
+// wyszukiwarka
+const searchInput = document.querySelector(".search-input");
+
+searchInput.addEventListener("input", () => {
+    const filteredProducts = products.filter(product => {
+        return product.name.toLowerCase().includes(searchInput.value.toLowerCase().trim()) ||
+            product.category.toLowerCase().includes(searchInput.value.toLowerCase().trim());
+    });
+
+    productsGrid.innerHTML = "";
+
+    if (filteredProducts.length === 0) {
+        productsGrid.innerHTML = "Nie znaleziono produktów.";
+    }
+
+    filteredProducts.forEach(product => {
+        const productCard = createProductCard(product);
+        productsGrid.appendChild(productCard);
+    });
+});
+
+function createProductCard(product) {
     const productCard = document.createElement("div");
     productCard.className = "product-card";
 
@@ -79,12 +137,12 @@ products.forEach(product => {
         productCard.appendChild(productAvailable);
         productCard.appendChild(addToCartButton);
 
-    productsGrid.appendChild(productCard);
-
     addToCartButton.addEventListener("click", () => {
         cart.push(product);
         console.log(cart);
 
         cartCountElement.textContent = cart.length;
     });
-});
+
+    return productCard;
+}
